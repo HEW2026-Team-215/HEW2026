decbbfbd8eacbf66b0041e8fdac81949083f51f0
NH-AT12C-AT 若林 <nhs240037@gmail.com>
2025-11-18 11:22:50 +0900

    🚧 とりあえず1枠だけ表示 & 抽選システム実装

--- HEW2026_Project/Collision.h
+++ HEW2026_Project/Collision.h
@@ -1,44 +1,90 @@
+/*********************************************************************
+ * \file   Collision.h
+ * \brief  衝突判定 Collision
+ * 
+ * \author AT12C-41 Kotetsu Wakabayashi
+ * \date   2025-11-13
+ *********************************************************************/
 #pragma once
-#include<DirectXMath.h>
-#include<math.h>
-#include<cmath>
-#include<algorithm>
+ //=====| Includes |=====//
+#include <DirectXMath.h>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
 
-class Collision {
+//=====| Class Definition |=====//
+class Collision
+{
 public:
-	//--- 当たり判定形状の種類
-	enum Type {
+	//--- 当たり判定形状の種類 Type of collision shapes.
+	enum Type
+	{
 		eNone,
-		eBox,
-		eSphere,
+		eBox,			// ボックス
+		eSphere,	// 球
+		ePlane,		// 平面
+		eRay,			// レイ
+		eLine,		// 線
+		ePoint,
+		eTriangle,
 	};
 
-	//--- 当たり判定の形状
-	struct Box {		// 四角
-		DirectX::XMFLOAT3		center;		// 中心座標
-		DirectX::XMFLOAT3		size;		// サイズ
+	//--- 当たり判定の形状 shape of collision.
+	struct Box
+	{
+		DirectX::XMFLOAT3 center;		// 中心座標
+		DirectX::XMFLOAT3 size;			// サイズ
 	};
-	struct Sphere {	// 球
-		DirectX::XMFLOAT3		center;		// 中心座標
-		float					radius;		// 半径
+	struct Sphere
+	{
+		DirectX::XMFLOAT3 center;		// 中心座標
+		float radius;								// 半径
+	};
+	struct Plane
+	{
+		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 normal;
+	};
+	struct Ray
+	{
+		DirectX::XMFLOAT3 origin;
+		DirectX::XMFLOAT3 dir;
+	};
+	struct Line
+	{
+		DirectX::XMFLOAT3 start;
+		DirectX::XMFLOAT3 end;
+	};
+	struct Point
+	{
+		DirectX::XMFLOAT3 pos;
+	};
+	struct Triangle
+	{
+		DirectX::XMFLOAT3 point[3];
 	};
 
-
-	//--- 当たり判定の統合情報
-	struct Info {
+	struct Info
+	{
 		Type type;
-		union {
+		union
+		{
 			Box box;
 			Sphere sphere;
+			Plane plane;
+			Ray ray;
+			Line line;
+			Point point;
+			Triangle triangle;
 		};
 	};
 
-	//--- 当たり判定の結果
-	struct Result {
-		bool					isHit;		// 当たったかどうか
-		DirectX::XMFLOAT3		point;		// ヒット位置
-		DirectX::XMFLOAT3		normal;		// ヒット平面
-		Info					other;		// 当たり判定オブジェクト
+	struct Result
+	{
+		bool isHit;								// 当たったかどうか
+		DirectX::XMFLOAT3 point;	// ヒット位置
+		DirectX::XMFLOAT3 normal;	// ヒット平面
+		Info other;								// 当たり判定オブジェクト
 	};
 
 public:
@@ -47,7 +93,8 @@ public:
 
 	// 四角同士の当たり判定
 	static Result Hit(Box a, Box b);
-
-	// 球同士の当たり判定
-	static Result Hit(Sphere a, Sphere b);
+	static Result Hit(Sphere z, Sphere b);
+	static Result Hit(Plane plane, Ray ray, float lenght);
+	static Result Hit(Plane plane, Line line);
+	static Result Hit(Point point, Triangle triangle);
 };
