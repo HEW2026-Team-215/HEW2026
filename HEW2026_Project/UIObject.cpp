4f36ab414ae9c9f4dd055b524126796c3949f853
NH-AT12C-AT 若林 <nhs240037@gmail.com>
2025-11-18 14:43:40 +0900

    🔧 projectファイル修正

--- HEW2026_Project/UIObject.cpp
+++ HEW2026_Project/UIObject.cpp
@@ -5,4 +5,8 @@
  * \author AT12C-41 Kotetsu Wakabayashi
  * \date   2025-11-18
  *********************************************************************/
+//=====| Includes |=====//
 #include "UIObject.h"
+#include "Defines.h"
+
+UIObject::UIObject() : UIObject("Placeholder", SCREEN_WIDTH * .5f, SCREEN_HEIGHT * .5f, 100.f, 100.f) {};
