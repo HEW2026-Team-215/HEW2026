b40e259f75e18167477a627cb2bfe0468ca22bf5
nhs240037 <238187822+nhs240037@users.noreply.github.com>
2025-11-21 13:33:33 +0900

    🚧 UV操作ガワだけ

--- HEW2026_Project/UIObject.h
+++ HEW2026_Project/UIObject.h
@@ -26,14 +26,20 @@ public:
 	void SetPosition(float X, float Y);
 	void SetSize(float W, float H);
 	void SetRotation(float X, float Y, float Z);
+	void SetUVPos(float X, float Y);
+	void SetUVScale(float X, float Y);
 
 	DirectX::XMFLOAT2 GetPosition(void);
 	DirectX::XMFLOAT2 GetSize(void);
 	DirectX::XMFLOAT3 GetRotation(void);
+	DirectX::XMFLOAT2 GetUVPos(void);
+	DirectX::XMFLOAT2 GetUVScale(void);
 
 private:
 	Texture* m_pTexture;
 	DirectX::XMFLOAT2 m_fPosition;
 	DirectX::XMFLOAT2 m_fSize;
 	DirectX::XMFLOAT3 m_fRotation;
+	DirectX::XMFLOAT2 m_fUVPos;
+	DirectX::XMFLOAT2 m_fUVSize;
 };
